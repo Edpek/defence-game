1d5e893c3bdfdc1989616ed244413f33b5ac99b1
Edpek <oyoosung98@gmail.com>
2024-04-22 14:49:05 +0900

    hp바 기능 삭제함
    2024.04.22

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,30 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100; // 최대 체력
     private int currentHealth; // 현재 체력
 
-    private HpBarFollow hpBarFollow; // HpBarFollow 스크립트 참조 변수
-
     private void Start()
     {
         currentHealth = maxHealth; // 시작할 때 최대 체력으로 초기화
-
-        // Enemy 오브젝트에서 HpBarFollow 스크립트를 찾아 참조합니다.
-        hpBarFollow = GetComponent<HpBarFollow>();
     }
 
     // 발사체에 의한 데미지 처리 메서드
     public void TakeDamage(int damage)
     {
         currentHealth -= damage; // 데미지를 체력에서 빼기
-
-        // 체력 바 업데이트
-        if (hpBarFollow != null)
-        {
-            hpBarFollow.UpdateHpBar(currentHealth, maxHealth);
-        }
 
         if (currentHealth <= 0)
         {
@@ -36,14 +26,6 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         // 적을 제거하거나 필요한 처리를 수행합니다.
-
-        // HpBarFollow 스크립트가 존재하면 HP 바를 파괴합니다.
-        if (hpBarFollow != null)
-        {
-            hpBarFollow.DestroyHpBar();
-        }
-
-        // 적 오브젝트를 파괴합니다.
-        Destroy(gameObject);
+        Destroy(gameObject); // 적 오브젝트를 파괴합니다.
     }
 }
